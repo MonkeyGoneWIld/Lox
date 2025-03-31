bb48a3b5c03aab0cfa1bb831f72adbd3cbed106f
redusys <redusys.ops@gmail.com>
2025-03-31 17:35:49 +0200

    Remove invalid token

--- salmon/release_notification.py
+++ salmon/release_notification.py
@@ -7,7 +7,7 @@ import requests
 from salmon import config
 
 LOCAL_VERSION_FILE = path.abspath(path.join(path.dirname(path.dirname(__file__)), "data", "version.py"))
-REMOTE_VERSION_URL = 'https://raw.githubusercontent.com/smokin-salmon/smoked-salmon/refs/heads/master/data/version.py?token=REDACTED-GH-TOKEN'
+REMOTE_VERSION_URL = 'https://raw.githubusercontent.com/smokin-salmon/smoked-salmon/refs/heads/master/data/version.py'
 
 def _extract_changelog(content, from_version, to_version):
     """Extracts the changelog entries between the specified versions."""
